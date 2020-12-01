abde70aebb04cdef4e65ebb437930cb5b96c0ed5
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-12-01 23:10:24 +0000

    chore: started set newUser

--- src/RepoList/index.jsx
+++ src/RepoList/index.jsx
@@ -4,25 +4,46 @@ import './styles.css';
 
 const RepoList = () => {
   const [repositories, setRepositories] = useState([]);
+  const [newUser, setNewUser] = useState('carvalhobfr');
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getRepos() {
       const response = await fetch(
-        'https://api.github.com/users/carvalhobfr/repos',
+        `https://api.github.com/users/${newUser}/repos`,
       );
       const data = await response.json();
 
       setRepositories(data);
     }
-
+    setLoading(true);
     getRepos();
-  }, []);
+    setLoading(false);
+  }, [newUser]);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setNewUser(newUser);
+  }
+
   return (
-    <ul>
-      {repositories.map((repo) => (
-        <li key={repo.id}>{repo.full_name}</li>
-      ))}
-    </ul>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newUser}
+        onChange={(e) => setNewUser(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo.id}>{repo.full_name}</li>
+        ))}
+      </ul>
+    </form>
   );
 };
 
