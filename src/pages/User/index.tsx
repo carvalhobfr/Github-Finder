295fa70501bdfd896177913ffee1ae3f9ba4b545
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-12-03 01:48:26 +0000

    feat: repos list done with map, done!

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { promises } from 'fs';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,11 +11,49 @@ import { Header, UserInfo, Repository } from './styles';
 import { Logo } from '../../styles/global';
 
 interface UserParams {
+  repository: any;
   user: string;
 }
 
+interface User {
+  login: string;
+  avatar_url: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  name: string;
+  bio: string;
+}
+
+interface Repository {
+  map(arg0: (repository: any) => JSX.Element): React.ReactNode;
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const User: React.FC = () => {
+  const [user, setUser] = useState<User>();
+  const [repositories, setRepository] = useState<Repository | null>(null);
+
   const { params } = useRouteMatch<UserParams>();
+
+  useEffect(() => {
+    api.get(`users/${params.user}`).then(response => {
+      setUser(response.data);
+    });
+
+    api.get(`users/${params.user}/repos`).then(response => {
+      setRepository(response.data);
+    });
+  }, [params.user]);
+
   return (
     <>
       <Header>
@@ -23,44 +63,51 @@ const User: React.FC = () => {
           Voltar
         </Link>
       </Header>
+      {user ? (
+        <UserInfo>
+          <header>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <h1>{user.name}</h1>
+              <h5>{user.login}</h5>
+              <strong>{user.bio}</strong>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{user.followers}</strong>
+              <span>Followers</span>
+            </li>
+            <li>
+              <strong>{user.following}</strong>
+              <span>Following</span>
+            </li>
+            <li>
+              <strong>{user.public_repos}</strong>
+              <span>Repositories</span>
+            </li>
+          </ul>
+        </UserInfo>
+      ) : (
+        <h1> Loading...</h1>
+      )}
+      {repositories ? (
+        <Repository>
+          {repositories.map(repository => (
+            <a key={repository.id} href={repository.html_url}>
+              <div>
+                <strong>{repository.name}</strong>
+                <p>{repository.description}</p>
+              </div>
 
-      <UserInfo>
-        <header>
-          <img
-            src="https://avatars3.githubusercontent.com/u/21340896?v=4"
-            alt="Matheus Carvalho"
-          />
-          <div>
-            <strong>Carvalhobfr</strong>
-            <p>Bio</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Followers</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Following</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Repos</span>
-          </li>
-        </ul>
-      </UserInfo>
-
-      <Repository>
-        <Link to="asdasd">
-          <div>
-            <strong>Test</strong>
-            <p>little test</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </Link>
-      </Repository>
+              <FiChevronRight size={20} />
+            </a>
+          ))}
+        </Repository>
+      ) : (
+        <h1> Loading...</h1>
+      )}
+      ;
     </>
   );
 };
